d72416894cf00257e2a81f190a11431eaded5618
sebfro <sebastian17pepp@gmail.com>
2018-11-05 10:47:57 +0100

    Update SpagettiMetoden/GlobalVariables.cs

--- SpagettiMetoden/GlobalVariables.cs
+++ SpagettiMetoden/GlobalVariables.cs
@@ -50,7 +50,6 @@ namespace SpagettiMetoden
         public static int day = 29;
 
         //Sannsynlighet for å velge en path som er nærmere "capture point"
-        public static double probability = 0.85;
         public static double Propability { get; set; }
     };
 
@@ -76,5 +75,4 @@ namespace SpagettiMetoden
             lock (_inst) return _inst.NextDouble() * (1 - 0.4) + 0.4;
         }
     }
-}
 
