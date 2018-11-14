32d8bd7244bd00e72acc58d602e8a619ad4b09c8
LAPTOP-51KKSN3E\sebas <sebastian17pepp@gmail.com>
2018-11-14 11:42:59 +0100

    Update SpagettiMetoden/RouteChooser.cs

--- SpagettiMetoden/RouteChooser.cs
+++ SpagettiMetoden/RouteChooser.cs
@@ -9,17 +9,28 @@ namespace SpagettiMetoden
 {
     class RouteChooser
     {
-        public double currDistanceFromCapture;
+        public double currDistanceFromCaptureOrRelease;
         public double randDouble;
+        public double captureOrReleaseLat;
+        public double captureOrReleaseLon;
         public double captureLat;
         public double captureLon;
 
         public RouteChooser(double lat, double lon, Fish currFish)
         {
-            currDistanceFromCapture = CalculateCoordinates.GetDistanceFromLatLonInKm(lat, lon, currFish.CaptureLat, currFish.CaptureLon);
+            currDistanceFromCaptureOrRelease = CalculateCoordinates.GetDistanceFromLatLonInKm(lat, lon, currFish.CaptureLat, currFish.CaptureLon);
             captureLat = currFish.CaptureLat;
             captureLon = currFish.CaptureLon;
+
         }
+
+        public RouteChooser(double lat, double lon, double currFishCaptureOrReleaseLat, double currFishCaptureOrReleaseLon)
+        {
+            currDistanceFromCaptureOrRelease = CalculateCoordinates.GetDistanceFromLatLonInKm(lat, lon, currFishCaptureOrReleaseLat, currFishCaptureOrReleaseLon);
+            captureOrReleaseLat = currFishCaptureOrReleaseLat;
+            captureOrReleaseLon = currFishCaptureOrReleaseLon;
+        }
+
         //TODO Vi må google eller snakke med veileder og finne ut om denne metoden er trådsikker
         public bool ChosenRoute(BlockingCollection<PositionData> validPositionsDataList, int randInt)
         {
@@ -27,10 +38,10 @@ namespace SpagettiMetoden
 
             double newDistanceFromCapture = CalculateCoordinates.GetDistanceFromLatLonInKm(
                                         validPositionsDataList.ElementAt(randInt).lat,
-                                        validPositionsDataList.ElementAt(randInt).lon, captureLat,
-                                        captureLon);
+                                        validPositionsDataList.ElementAt(randInt).lon, captureOrReleaseLat,
+                                        captureOrReleaseLon);
             double weight = GlobalVariables.Probability;
-            return (newDistanceFromCapture < currDistanceFromCapture && randDouble < weight || newDistanceFromCapture >= currDistanceFromCapture && randDouble >= weight);
+            return (newDistanceFromCapture < currDistanceFromCaptureOrRelease && randDouble < weight || newDistanceFromCapture >= currDistanceFromCaptureOrRelease && randDouble >= weight);
         }
     }
 }
